7b4ac46bab6d82020914cdd48bef80d729097a8f
hemanth makaraju <hemanth.makaraju@emc.com>
2016-06-04 21:57:23 +0530

    added cleanup module

--- coprhd_flocker_plugin/testtools_emc_coprhd.py
+++ coprhd_flocker_plugin/testtools_emc_coprhd.py
@@ -10,6 +10,7 @@ import os
 import yaml
 import socket
 from uuid import uuid4
+from uuid import UUID, uuid4
 
 from twisted.trial.unittest import SkipTest
 
@@ -24,7 +25,20 @@ def _read_coprhd_yaml():
     with open(config_file_path, 'r') as stream:
         coprhd_conf = yaml.load(stream)
     return coprhd_conf['dataset']
-    
+
+def detach_destroy_volumes(cliobj):
+    """
+    Detach and destroy all volumes known.
+    :param : cliobj
+    """
+    volumes_dict = cliobj.list_volume()
+    if volumes_dict is not None:
+       for volume_name,volume_attr in volumes_dict.iteritems():
+         dataset_id=UUID(volume_name)
+         if volume_attr['attached_to'] is not None:
+           cliobj.unexport_volume("flocker-{}".format(dataset_id))
+         cliobj.delete_volume("flocker-{}".format(dataset_id)) 
+         
 def tidy_coprhd_client_for_test(test_case):
     """
     Return a ``CoprHD Client`and register a ``test_case``
@@ -47,5 +61,6 @@ def tidy_coprhd_client_for_test(test_case):
     coprhdcli_security_file = dataset['coprhdcli_security_file']
         
     coprhdobj = configuration(coprhdhost, port, tenant,project, varray, cookiedir, vpool,vpool_platinum,vpool_gold,vpool_silver,vpool_bronze,hostexportgroup,coprhdcli_security_file)
+    test_case.addCleanup(detach_destroy_volumes, coprhdobj.coprhdcli)
     return coprhdobj
       
